c397527f1538760c2b0dc47034ab410396114822
Kashvi <kz9467@srmist.edu.in>
2025-10-03 23:50:33 +0530

    Add files via upload

---
+++ test_script.py
@@ -0,0 +1,7 @@
+import tensorflow as tf # type: ignore
+import cv2 # type: ignore
+import numpy as np # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+
+print("✅ All libraries loaded successfully!")
+
